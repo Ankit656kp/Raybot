20a4e8b07359182bb91578276acd77a94b96d90b
Ankit kumar <ankitgupta2ndsp@gmail.com>
2026-01-09 18:38:14 +0530

    Swap SUPPORT_CHANNEL and SUPPORT_CHAT URLs

--- config.py
+++ config.py
@@ -39,8 +39,8 @@ GIT_TOKEN = getenv("GIT_TOKEN", None)
 
 # ===============================================
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/BoysAndGirlZone")
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/xclusor")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/xclusor")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/BoysAndGirlZone")
 
 # ===============================================
 
